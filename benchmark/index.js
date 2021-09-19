0814facfb74d5ff5aa21aa1106ac52cd2fd50c09
Jonas Hermsmeier <jhermsmeier@gmail.com>
2021-09-19 12:48:09 +0200

    package: Update benchmarks

--- benchmark/index.js
+++ benchmark/index.js
@@ -3,11 +3,11 @@ var fs = require( 'fs' )
 var bench = require( 'nanobench' )
 var crc32 = require( '..' )
 
-const ITERATIONS = 100000
+const ITERATIONS = 1000
 
-bench( `crc32( 512B ) ⨉ ${ITERATIONS}`, function( run ) {
+bench( `crc32( 1MB ) ⨉ ${ITERATIONS}`, function( run ) {
 
-  var buffer = crypto.randomBytes(512)
+  var buffer = crypto.randomBytes(1 * 1024 * 1024)
 
   run.start()
   for( var i = 0; i < ITERATIONS; i++ ) {
@@ -17,44 +17,60 @@ bench( `crc32( 512B ) ⨉ ${ITERATIONS}`, function( run ) {
 
 })
 
-// ;[
-//   { name: 'crc32', fn: require('crc32') },
-//   { name: 'buffer-crc32', fn: require('buffer-crc32') },
-//   { name: 'crc-32', fn: require('crc-32').buf },
-//   { name: 'sse4_crc32', fn: require('sse4_crc32').calculate },
-// ].forEach( function( lib ) {
-//   bench( `${lib.name}( 512B ) ⨉ ${ITERATIONS}`, function( run ) {
-//     var buffer = crypto.randomBytes(512)
-//     run.start()
-//     for( var i = 0; i < ITERATIONS; i++ ) {
-//       checksum = lib.fn( buffer )
-//     }
-//     run.end()
-//   })
-// })
+bench( `crc32c( 1MB ) ⨉ ${ITERATIONS}`, function( run ) {
 
-bench( `crc32( 1MB ) ⨉ ${1}`, function( run ) {
-
-  var buffer = crypto.randomBytes(1024 * 1024)
+  var buffer = crypto.randomBytes(1 * 1024 * 1024)
 
   run.start()
-  for( var i = 0; i < 1; i++ ) {
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    checksum = crc32( buffer, 0, crc32.TABLE.CASTAGNOLI )
+  }
+  run.end()
+
+})
+
+;[
+  { name: 'crc32', fn: require('crc32') },
+  { name: 'buffer-crc32', fn: require('buffer-crc32') },
+  { name: 'crc-32', fn: require('crc-32').buf },
+  { name: 'polycrc', fn: require('polycrc').crc32 },
+  { name: 'sse4_crc32', fn: require('sse4_crc32').calculate },
+].forEach( function( lib ) {
+  bench( `extern:${lib.name}( 1MB ) ⨉ ${ITERATIONS}`, function( run ) {
+    var buffer = crypto.randomBytes(1 * 1024 * 1024)
+    run.start()
+    for( var i = 0; i < ITERATIONS; i++ ) {
+      checksum = lib.fn( buffer )
+    }
+    run.end()
+  })
+})
+
+bench( `crc32( 1MB ) ⨉ ${ITERATIONS}`, function( run ) {
+
+  var buffer = crypto.randomBytes(1 * 1024 * 1024)
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
     checksum = crc32( buffer )
   }
   run.end()
 
 })
 
-bench( `crc32.Hash() [1MB] ⨉ ${1}`, function( run ) {
+bench( `crc32.Hash() [1000MB] ⨉ ${1}`, function( run ) {
 
-  var readStream = fs.createReadStream( __dirname + '/../test/data/random.bin' )
   var checksum = new crc32.Hash()
-
-  readStream.pipe( checksum )
-    .on( 'readable', function() {
-      run.end()
-    })
+  var readStream = fs.createReadStream( '/dev/urandom', {
+    end: 1000 * 1024 * 1024,
+  })
 
   run.start()
+
+  readStream.pipe( checksum )
+    .on( 'end', function() {
+      run.end()
+    })
+    .resume()
 
 })
